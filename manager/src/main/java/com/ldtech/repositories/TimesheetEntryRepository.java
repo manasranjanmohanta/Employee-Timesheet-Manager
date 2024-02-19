132b44eb23ec71ff8443191a63fde1cefb11629f
Manas Ranjan Mohanta <manasranjanmohanta679@gmail.com>
2024-02-19 17:08:32 +0530

    Added API's for home page to fetch data with approval status from previous week to current date

--- manager/src/main/java/com/ldtech/repositories/TimesheetEntryRepository.java
+++ manager/src/main/java/com/ldtech/repositories/TimesheetEntryRepository.java
@@ -1,9 +1,20 @@
 package com.ldtech.repositories;
 
+import com.ldtech.entities.Employee;
 import com.ldtech.entities.TimesheetEntry;
 import com.ldtech.entities.TimesheetEntryId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface TimesheetEntryRepository extends JpaRepository<TimesheetEntry, TimesheetEntryId> {
+//    @Query("SELECT DISTINCT te.employee FROM TimesheetEntry te WHERE te.id.logDate = :logDate")
+//    List<Employee> findEmployeesByLogDate(LocalDate logDate);
+    @Query("SELECT te FROM TimesheetEntry te WHERE te.id.logDate = :logDate")
+    List<TimesheetEntry> findByLogDate(@Param("logDate") LocalDate logDate);
+
     // You can add custom query methods here if needed
 }
