e89eb032af21aa4d13e0ba422dcd9f96304da100
Manas Ranjan Mohanta <manasranjanmohanta679@gmail.com>
2024-03-01 10:49:59 +0530

    Added Gloabal CorsConfiguration

--- manager/src/main/java/com/ldtech/controllers/ActivityAllocationController.java
+++ manager/src/main/java/com/ldtech/controllers/ActivityAllocationController.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/activityAllocations")
-@CrossOrigin("*")
 public class ActivityAllocationController {
 
     @Autowired
--- manager/src/main/java/com/ldtech/controllers/HomeController.java
+++ manager/src/main/java/com/ldtech/controllers/HomeController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/home")
-@CrossOrigin("*")
 public class HomeController {
     @Autowired
     private HomeService homeService;
